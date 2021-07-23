ac8b956d3851a7413339c9e159d480325e6c92e7
YeungKC <I@YeungKC.com>
2021-07-23 17:20:50 +0800

    Refactor env

--- lib/service/env.dart
+++ lib/service/env.dart
@@ -3,8 +3,19 @@ import 'package:flutter/foundation.dart';
 
 part 'env.g.dart';
 
-@Envify(path: kReleaseMode ? '.production.env' : '.development.env')
-abstract class Env {
-  static const clientId = _Env.clientId;
-  static const clientSecret = _Env.clientSecret;
+@Envify(path: '.development.env')
+abstract class _DevEnv {
+  static const clientId = __DevEnv.clientId;
+  static const clientSecret = __DevEnv.clientSecret;
+}
+
+@Envify(path: '.production.env')
+abstract class _ProdEnv {
+  static const clientId = __ProdEnv.clientId;
+  static const clientSecret = __ProdEnv.clientSecret;
+}
+
+class Env {
+  static const clientId = kReleaseMode ? _ProdEnv.clientId : _DevEnv.clientId;
+  static const clientSecret = kReleaseMode ? _ProdEnv.clientSecret : _DevEnv.clientSecret;
 }
