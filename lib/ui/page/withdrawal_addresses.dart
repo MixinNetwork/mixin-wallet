2cd1f46a19e4d867bf30fe04517e2e112687b6a0
boyan <yangbinyhbn@gmail.com>
2021-07-28 10:59:53 +0800

    improve withdrawal addresses page style

--- lib/ui/page/withdrawal_addresses.dart
+++ lib/ui/page/withdrawal_addresses.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:intl/intl.dart';
 import 'package:mixin_bot_sdk_dart/mixin_bot_sdk_dart.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -12,7 +12,6 @@ import '../../util/constants.dart';
 import '../../util/extension/extension.dart';
 import '../../util/hook.dart';
 import '../../util/logger.dart';
-import '../../util/r.dart';
 import '../widget/address_add_widget.dart';
 import '../widget/mixin_appbar.dart';
 import '../widget/mixin_bottom_sheet.dart';
@@ -58,55 +57,57 @@ class WithdrawalAddresses extends HookWidget {
         ),
         backgroundColor: context.theme.accent,
         body: Container(
-          padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
           decoration: BoxDecoration(
             borderRadius:
                 const BorderRadius.vertical(top: Radius.circular(topRadius)),
             color: context.theme.background,
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const SizedBox(height: 12),
-              SearchHeaderWidget(
-                hintText: context.l10n.addressSearchHint,
-                onChanged: (k) {
-                  filterKeywords.value = k;
-                },
-                controller: controller,
-                cancelVisible: false,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: SearchHeaderWidget(
+                  hintText: context.l10n.addressSearchHint,
+                  onChanged: (k) {
+                    filterKeywords.value = k;
+                  },
+                  controller: controller,
+                  cancelVisible: false,
+                ),
               ),
-              const SizedBox(height: 22),
+              const SizedBox(height: 10),
               Expanded(child: _WithdrawalAddressList(addresses: filterList)),
-              Align(
-                  alignment: Alignment.bottomCenter,
-                  child: SizedBox(
-                      width: 160,
-                      height: 44,
-                      child: TextButton(
-                          style: ButtonStyle(
-                              padding: MaterialStateProperty.all<EdgeInsets>(
-                                  const EdgeInsets.symmetric(
-                                      horizontal: 22, vertical: 11)),
-                              shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
+              const SizedBox(height: 10),
+              SizedBox(
+                  width: 160,
+                  height: 44,
+                  child: TextButton(
+                      style: ButtonStyle(
+                          padding: MaterialStateProperty.all<EdgeInsets>(
+                              const EdgeInsets.symmetric(
+                                  horizontal: 22, vertical: 11)),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
                                   RoundedRectangleBorder(
                                       borderRadius: BorderRadius.circular(12.0),
                                       side: BorderSide(
                                           color: context.theme.accent)))),
-                          onPressed: () {
-                            showMixinBottomSheet(
-                              context: context,
-                              builder: (context) =>
-                                  AddressAddWidget(assetId: assetId),
-                              isScrollControlled: true,
-                            );
-                          },
-                          child: Text('+ ${context.l10n.addAddress}',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: context.theme.accent,
-                              ))))),
-              const SizedBox(height: 70),
+                      onPressed: () {
+                        showMixinBottomSheet(
+                          context: context,
+                          builder: (context) =>
+                              AddressAddWidget(assetId: assetId),
+                          isScrollControlled: true,
+                        );
+                      },
+                      child: Text('+ ${context.l10n.addAddress}',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: context.theme.accent,
+                          )))),
+              const SizedBox(height: 20),
             ],
           ),
         ));
@@ -165,32 +166,36 @@ class _Item extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
         child: InkWell(
           onTap: () {},
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ClipOval(
-                child: SvgPicture.asset(
-                  R.resourcesTransactionNetSvg,
-                  height: 44,
-                  width: 44,
-                ),
-              ),
-              const SizedBox(width: 12),
               Expanded(
                   child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Text(
-                    address.label.overflow,
-                    style: TextStyle(
-                        color: context.theme.text,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                        height: 1.4),
+                  Row(
+                    children: [
+                      Text(
+                        address.label.overflow,
+                        style: TextStyle(
+                            color: context.theme.text,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            height: 1.4),
+                      ),
+                      const Spacer(),
+                      Text(
+                        DateFormat.yMMMd().format(address.updatedAt),
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: context.theme.secondaryText,
+                        ),
+                      ),
+                    ],
                   ),
                   const SizedBox(height: 4),
                   Text(
@@ -205,7 +210,6 @@ class _Item extends StatelessWidget {
                   ),
                 ],
               )),
-              const SizedBox(width: 48),
             ],
           ),
         ),
