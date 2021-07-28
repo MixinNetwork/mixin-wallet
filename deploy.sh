e98795d826d4c33f2f04fa328b7de075b966c770
Crossle Song <crosslesong@gmail.com>
2021-07-28 16:17:13 +0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,10 @@
 
 rm -r build/web/*
 sed -i -- "s/BUILD_VERSION/`git rev-parse HEAD`/g" web/index.html || exit
-flutter pub run build_runner build --delete-conflicting-outputs || exit
+#flutter pub run build_runner build --delete-conflicting-outputs || exit
 flutter build web --web-renderer html --release || exit
+
+ssh one@mixin-wallet cp -r /home/one/apps/wallet/web /home/one/apps/wallet/web_old
 
 rsync -rcv build/web/* one@mixin-wallet:/home/one/apps/wallet/web/
 
