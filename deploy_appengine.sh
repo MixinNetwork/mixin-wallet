4c5e381c82df7f3f377dc18899c65b7beb2fc362
Crossle Song <crosslesong@gmail.com>
2021-08-06 18:27:38 +0400

    Update deploy sh

--- deploy_appengine.sh
+++ deploy_appengine.sh
@@ -2,7 +2,7 @@
 
 rm -r build/web/*
 
-sed -i -- "s/BUILD_VERSION/`git rev-parse HEAD`/g" web/index.html || exit
+sed -i '' "s/BUILD_VERSION/`git rev-parse HEAD`/g" web/index.html || exit
 #flutter pub run build_runner build --delete-conflicting-outputs || exit
 flutter build web --web-renderer html --release || exit
 
@@ -12,4 +12,6 @@ mkdir dist
 cp -r build/web dist/
 
 cp app.one.yaml dist/app.yaml
-cd dist && gcloud app deploy app.yaml
+gcloud app deploy dist/app.yaml
+
+git checkout web/index.html
