534c53b42dfa29bbf89eeea3dbdbeec2eade0ee1
boyan <17426470+boyan01@users.noreply.github.com>
2021-08-26 14:55:29 +0800

    refactor home header button widget to HeaderButtonBarLayout.

--- lib/ui/widget/buttons.dart
+++ lib/ui/widget/buttons.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 import '../../util/extension/extension.dart';
 import '../../util/r.dart';
@@ -49,4 +51,82 @@ class MixinBackButton2 extends StatelessWidget {
           },
         ),
       );
+}
+
+class HeaderButtonBarLayout extends StatelessWidget {
+  const HeaderButtonBarLayout({
+    Key? key,
+    required this.buttons,
+  }) : super(key: key);
+
+  final List<HeaderButton> buttons;
+
+  @override
+  Widget build(BuildContext context) => Material(
+        color: context.colorScheme.surface,
+        borderRadius: BorderRadius.circular(8),
+        child: SizedBox(
+          height: 40,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ...buttons
+                  .map<Widget>((e) => Expanded(child: e))
+                  .separated(_divider(context))
+            ],
+          ),
+        ),
+      );
+
+  Widget _divider(BuildContext context) => Container(
+        width: 2,
+        height: 24,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(1),
+          color: context.colorScheme.background,
+        ),
+      );
+}
+
+class HeaderButton extends HookWidget {
+  const HeaderButton({
+    Key? key,
+    required this.child,
+    required this.onTap,
+  }) : super(key: key);
+
+  final Widget child;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final animator = useAnimationController(
+      duration: const Duration(milliseconds: 150),
+      initialValue: 1,
+      lowerBound: 0.3,
+      upperBound: 1,
+    );
+    useAnimation(animator);
+    return GestureDetector(
+      onTap: onTap,
+      onTapDown: (_) => animator.reverse(),
+      onTapUp: (_) => animator.forward(),
+      onTapCancel: animator.forward,
+      behavior: HitTestBehavior.opaque,
+      child: Center(
+        child: DefaultTextStyle(
+          style: TextStyle(
+            color: context.colorScheme.primaryText,
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+          child: Opacity(
+            opacity: animator.value,
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
 }
