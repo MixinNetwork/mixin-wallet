2a8e4d296443857c8ee9ca7f7182af3d5af3a702
boyan <17426470+boyan01@users.noreply.github.com>
2021-09-02 17:29:51 +0800

    fix format

--- lib/util/extension/src/async.dart
+++ lib/util/extension/src/async.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/widgets.dart';
 
 extension AsyncSnapshotExt on AsyncSnapshot {
-
   bool get isNoneOrWaiting =>
       connectionState == ConnectionState.none ||
       connectionState == ConnectionState.waiting;
