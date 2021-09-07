7133d5275122226d80b3549bbae12bbba0f9acb6
touge <touge1027@gmail.com>
2021-09-07 15:19:37 +0800

    Code format

--- lib/ui/widget/transaction_info_tile.dart
+++ lib/ui/widget/transaction_info_tile.dart
@@ -13,28 +13,28 @@ class TransactionInfoTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Column(
-    mainAxisSize: MainAxisSize.min,
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      const SizedBox(height: 12),
-      DefaultTextStyle(
-        style: TextStyle(
-          fontSize: 16,
-          height: 1,
-          color: context.colorScheme.thirdText,
-        ),
-        child: title,
-      ),
-      const SizedBox(height: 8),
-      DefaultTextStyle(
-        style: TextStyle(
-          fontSize: 16,
-          height: 1,
-          color: context.colorScheme.primaryText,
-        ),
-        child: subtitle,
-      ),
-      const SizedBox(height: 12),
-    ],
-  );
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 12),
+          DefaultTextStyle(
+            style: TextStyle(
+              fontSize: 16,
+              height: 1,
+              color: context.colorScheme.thirdText,
+            ),
+            child: title,
+          ),
+          const SizedBox(height: 8),
+          DefaultTextStyle(
+            style: TextStyle(
+              fontSize: 16,
+              height: 1,
+              color: context.colorScheme.primaryText,
+            ),
+            child: subtitle,
+          ),
+          const SizedBox(height: 12),
+        ],
+      );
 }
