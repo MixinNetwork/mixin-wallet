7ddabef7a938d5ed8f7d174480e8fb97fbeef3bb
Crossle Song <crosslesong@gmail.com>
2021-09-12 20:38:13 +0800

    More unused debug

--- lib/util/mixin_context.dart
+++ lib/util/mixin_context.dart
@@ -4,7 +4,6 @@ import 'dart:js' as js;
 
 Map<String, dynamic> getMixinContext() {
   final mixinContext = js.context['MixinContext'] as js.JsObject;
-  js.context.callMethod('alert', ['mixinContext: $mixinContext']);
   return jsonDecode(mixinContext.callMethod('getContext').toString())
       as Map<String, dynamic>;
 }
