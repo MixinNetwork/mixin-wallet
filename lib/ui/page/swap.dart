40e2882092f4efcfd2181c5f92d07380a4712575
boyan <17426470+boyan01@users.noreply.github.com>
2021-09-13 19:59:40 +0800

    fix uis.

--- lib/ui/page/swap.dart
+++ lib/ui/page/swap.dart
@@ -199,19 +199,15 @@ class _Body extends HookWidget {
                   await setSourceAssetId(sourceAsset.value.assetId);
                   await setDestAssetId(destAsset.value.assetId);
                 },
-                child: SizedBox.square(
+                child: Center(
+                  child: SizedBox.square(
                     dimension: 40,
-                    child: ClipOval(
-                        child: DecoratedBox(
-                      decoration: const BoxDecoration(
-                        color: Color(0xfff8f8f8),
-                      ),
-                      child: Center(
-                          child: SvgPicture.asset(
-                        R.resourcesIcSwitchSvg,
-                        color: context.colorScheme.primaryText,
-                      )),
-                    )))),
+                    child: SvgPicture.asset(
+                      R.resourcesIcSwitchSvg,
+                      color: context.colorScheme.primaryText,
+                    ),
+                  ),
+                )),
           ]),
           const SizedBox(height: 12),
           _AssetItem(
@@ -346,9 +342,10 @@ class _AssetItem extends HookWidget {
               symbolUrl: asset.value.iconUrl,
               chainUrl: asset.value.chainIconUrl,
               size: 40,
-              chainBorder: const BorderSide(color: Color(0xfff8f8f8), width: 1),
-              symbolBorder:
-                  const BorderSide(color: Color(0xfff8f8f8), width: 1.5),
+              chainBorder: const BorderSide(
+                color: Color(0xfff8f8f8),
+                width: 1.5,
+              ),
               chainSize: 14,
             )),
         const SizedBox(width: 10),
