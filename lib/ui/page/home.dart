3b4c7a09bd2d12c111eeab266c92d8779c90f546
Bin <17426470+boyan01@users.noreply.github.com>
2022-04-19 11:44:11 +0800

    clear database if remove authorize (#107)

--- lib/ui/page/home.dart
+++ lib/ui/page/home.dart
@@ -1,9 +1,9 @@
 import 'package:decimal/decimal.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:mixin_bot_sdk_dart/mixin_bot_sdk_dart.dart';
 
 import '../../db/mixin_database.dart';
+import '../../db/web/construct_db.dart';
 import '../../service/profile/profile_manager.dart';
 import '../../util/constants.dart';
 import '../../util/extension/extension.dart';
@@ -74,7 +74,7 @@ class Home extends HookWidget {
     final selectedTab = _Tab.values.byNameOrNull(tabParam) ?? _Tab.coins;
     return Scaffold(
       backgroundColor: context.theme.background,
-      appBar: _HomeAppBar(account: auth!.account),
+      appBar: const _HomeAppBar(),
       body: CustomScrollView(
         slivers: [
           SliverToBoxAdapter(
@@ -102,42 +102,42 @@ class Home extends HookWidget {
 }
 
 class _HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const _HomeAppBar({
-    Key? key,
-    required this.account,
-  }) : super(key: key);
-
-  final Account account;
+  const _HomeAppBar({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => MixinAppBar(
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 12),
-          child: Center(
-            child: InkResponse(
-              radius: 24,
-              onTap: () => showMixinBottomSheet(
-                context: context,
-                builder: (context) => const _AccountBottomSheet(),
-              ),
-              child: Avatar(
-                avatarUrl: account.avatarUrl,
-                userId: account.userId,
-                name: account.fullName ?? '',
-                size: 32,
-              ),
+  Widget build(BuildContext context) {
+    final account = auth?.account;
+    return MixinAppBar(
+      leading: Padding(
+        padding: const EdgeInsets.only(left: 12),
+        child: Center(
+          child: InkResponse(
+            radius: 24,
+            onTap: () => showMixinBottomSheet(
+              context: context,
+              builder: (context) => const _AccountBottomSheet(),
             ),
+            child: account == null
+                ? const SizedBox()
+                : Avatar(
+                    avatarUrl: account.avatarUrl,
+                    userId: account.userId,
+                    name: account.fullName ?? '',
+                    size: 32,
+                  ),
           ),
         ),
-        actions: [
-          ActionButton(
-            name: R.resourcesSettingSvg,
-            size: 24,
-            onTap: () => context.push(settingPath),
-          )
-        ],
-        backgroundColor: context.colorScheme.background,
-      );
+      ),
+      actions: [
+        ActionButton(
+          name: R.resourcesSettingSvg,
+          size: 24,
+          onTap: () => context.push(settingPath),
+        )
+      ],
+      backgroundColor: context.colorScheme.background,
+    );
+  }
 
   @override
   Size get preferredSize => const Size.fromHeight(48);
@@ -213,7 +213,9 @@ class _AccountBottomSheet extends StatelessWidget {
             ),
           ),
           onTap: () async {
+            final id = auth!.account.identityNumber;
             await profileBox.clear();
+            await deleteDatabase(id);
             context.replace(authUri.path);
           },
         ),
