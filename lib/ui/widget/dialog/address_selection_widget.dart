8886ad00a1b2744f939eb5f01a827dc9aebb321e
Bin <17426470+boyan01@users.noreply.github.com>
2023-01-11 16:26:02 +0800

    address show delete popup menu after long press (#218)

--- lib/ui/widget/dialog/address_selection_widget.dart
+++ lib/ui/widget/dialog/address_selection_widget.dart
@@ -123,45 +123,8 @@ class _AddressItem extends StatelessWidget {
   final String? selectedAddressId;
 
   @override
-  Widget build(BuildContext context) => _SwipeToDismiss(
-        key: ValueKey(address.addressId),
-        onDismiss: () {
-          context.appServices.mixinDatabase.addressDao.deleteAddress(address);
-        },
-        confirmDismiss: (direction) {
-          if (isLoginByCredential) {
-            return showDeleteAddressByPinBottomSheet(
-              context,
-              address: address,
-            );
-          } else {
-            // https: //mixin.one/address?action=delete&asset=xxx&address=xxx
-            final uri = Uri.https('mixin.one', 'address', {
-              'action': 'delete',
-              'asset': address.assetId,
-              'address': address.addressId,
-            });
-            return showAndWaitingExternalAction(
-                context: context,
-                uri: uri,
-                hint: Text(context.l10n.delete),
-                action: () async {
-                  try {
-                    await context.appServices.client.addressApi
-                        .getAddressById(address.addressId);
-                    return false;
-                  } catch (error) {
-                    if (error is DioError) {
-                      final mixinError = error.error as sdk.MixinError;
-                      if (mixinError.code == 404) {
-                        return true;
-                      }
-                    }
-                    rethrow;
-                  }
-                });
-          }
-        },
+  Widget build(BuildContext context) => _AddressPopupMenuWrapper(
+        address: address,
         child: _AddressSelectionItemTile(
           onTap: () => Navigator.pop(context, address),
           title: Row(
@@ -185,6 +148,112 @@ class _AddressItem extends StatelessWidget {
       );
 }
 
+enum _AddressPopupMenuAction {
+  delete,
+}
+
+class _AddressPopupMenuWrapper extends StatefulWidget {
+  const _AddressPopupMenuWrapper({
+    Key? key,
+    required this.child,
+    required this.address,
+  }) : super(key: key);
+
+  final Widget child;
+  final Addresse address;
+
+  @override
+  State<_AddressPopupMenuWrapper> createState() =>
+      _AddressPopupMenuWrapperState();
+}
+
+class _AddressPopupMenuWrapperState extends State<_AddressPopupMenuWrapper> {
+  Future<void> showPopupMenu({Offset? popupOffset}) async {
+    const padding = EdgeInsets.all(8.0);
+    final popupMenuTheme = PopupMenuTheme.of(context);
+    final button = context.findRenderObject()! as RenderBox;
+    final overlay =
+        Navigator.of(context).overlay!.context.findRenderObject()! as RenderBox;
+    final offset =
+        popupOffset ?? Offset(0.0, button.size.height - (padding.vertical / 2));
+    final position = RelativeRect.fromRect(
+      Rect.fromPoints(
+        button.localToGlobal(offset, ancestor: overlay),
+        button.localToGlobal(button.size.topLeft(Offset.zero) + offset,
+            ancestor: overlay),
+      ),
+      Offset.zero & overlay.size,
+    );
+    final action = await showMenu<_AddressPopupMenuAction>(
+      context: context,
+      elevation: popupMenuTheme.elevation,
+      items: [
+        PopupMenuItem(
+          value: _AddressPopupMenuAction.delete,
+          child: Text(context.l10n.delete),
+        ),
+      ],
+      position: position,
+      shape: popupMenuTheme.shape,
+      color: popupMenuTheme.color,
+    );
+    switch (action) {
+      case null:
+        return;
+      case _AddressPopupMenuAction.delete:
+        await _deleteAddress();
+        break;
+    }
+  }
+
+  Addresse get address => widget.address;
+
+  Future<bool> _confirmDeleteAddress() async {
+    if (isLoginByCredential) {
+      return showDeleteAddressByPinBottomSheet(
+        context,
+        address: address,
+      );
+    } else {
+      // https: //mixin.one/address?action=delete&asset=xxx&address=xxx
+      final uri = Uri.https('mixin.one', 'address', {
+        'action': 'delete',
+        'asset': address.assetId,
+        'address': address.addressId,
+      });
+      return showAndWaitingExternalAction(
+          context: context,
+          uri: uri,
+          hint: Text(context.l10n.delete),
+          action: () async {
+            try {
+              await context.appServices.client.addressApi
+                  .getAddressById(address.addressId);
+              return false;
+            } catch (error) {
+              if (error is DioError) {
+                final mixinError = error.error as sdk.MixinError;
+                if (mixinError.code == 404) {
+                  return true;
+                }
+              }
+              rethrow;
+            }
+          });
+    }
+  }
+
+  Future<void> _deleteAddress() async {
+    final delete = await _confirmDeleteAddress();
+    if (delete) {
+      await context.appServices.mixinDatabase.addressDao.deleteAddress(address);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.child;
+}
+
 class _AddressSelectionItemTile extends StatelessWidget {
   const _AddressSelectionItemTile({
     Key? key,
@@ -203,102 +272,62 @@ class _AddressSelectionItemTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) => InkWell(
         onTap: onTap,
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(minHeight: 72),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                if (selected)
-                  Padding(
-                    padding: const EdgeInsets.only(left: 0, right: 10),
-                    child: SvgPicture.asset(
-                      R.resourcesIcCheckSvg,
-                      width: 24,
-                      height: 24,
+        child: GestureDetector(
+          onLongPressStart: (details) {
+            debugPrint('offset: ${details.localPosition}');
+            context
+                .findAncestorStateOfType<_AddressPopupMenuWrapperState>()
+                ?.showPopupMenu(popupOffset: details.localPosition);
+          },
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(minHeight: 72),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  if (selected)
+                    Padding(
+                      padding: const EdgeInsets.only(left: 0, right: 10),
+                      child: SvgPicture.asset(
+                        R.resourcesIcCheckSvg,
+                        width: 24,
+                        height: 24,
+                      ),
+                    )
+                  else
+                    const SizedBox(width: 34),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        DefaultTextStyle.merge(
+                          style: TextStyle(
+                            color: context.colorScheme.primaryText,
+                            fontSize: 16,
+                            height: 1.2,
+                          ),
+                          child: title,
+                        ),
+                        const SizedBox(height: 2),
+                        DefaultTextStyle.merge(
+                          softWrap: true,
+                          style: TextStyle(
+                            color: context.colorScheme.thirdText,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            height: 1.2,
+                          ),
+                          child: subtitle,
+                        ),
+                      ],
                     ),
-                  )
-                else
-                  const SizedBox(width: 34),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      DefaultTextStyle.merge(
-                        style: TextStyle(
-                          color: context.colorScheme.primaryText,
-                          fontSize: 16,
-                          height: 1.2,
-                        ),
-                        child: title,
-                      ),
-                      const SizedBox(height: 2),
-                      DefaultTextStyle.merge(
-                        softWrap: true,
-                        style: TextStyle(
-                          color: context.colorScheme.thirdText,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          height: 1.2,
-                        ),
-                        child: subtitle,
-                      ),
-                    ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
       );
-}
-
-class _SwipeToDismiss extends StatelessWidget {
-  const _SwipeToDismiss({
-    required Key key,
-    required this.child,
-    required this.onDismiss,
-    this.confirmDismiss,
-  }) : super(key: key);
-
-  final Widget child;
-  final VoidCallback onDismiss;
-  final ConfirmDismissCallback? confirmDismiss;
-
-  @override
-  Widget build(BuildContext context) {
-    final Widget indicator = Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 18),
-      child: Text(
-        context.l10n.delete,
-        style: const TextStyle(
-          fontWeight: FontWeight.w500,
-          fontSize: 16,
-          color: Colors.white,
-        ),
-      ),
-    );
-    return Dismissible(
-      key: ValueKey(key),
-      onDismissed: (direction) => onDismiss(),
-      confirmDismiss: confirmDismiss,
-      background: ColoredBox(
-        color: context.colorScheme.red,
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child: indicator,
-        ),
-      ),
-      secondaryBackground: ColoredBox(
-        color: context.colorScheme.red,
-        child: Align(
-          alignment: Alignment.centerRight,
-          child: indicator,
-        ),
-      ),
-      child: child,
-    );
-  }
 }
