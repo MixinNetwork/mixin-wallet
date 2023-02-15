e336ce5aead4d7f7bbe6ff71726b3dfd1ba2d983
bin <17426470+boyan01@users.noreply.github.com>
2023-02-15 11:23:54 +0800

    add test parse ethereum

--- lib/util/pay/erc_681.dart
+++ lib/util/pay/erc_681.dart
@@ -5,6 +5,8 @@ import 'common_uri.dart';
 import 'erc_831.dart';
 import 'ethereum.dart';
 
+final _scientificNumberRegEx = RegExp(r'^\d+(\.\d+)?(e\d+)?$');
+
 class ERC681 {
   ERC681();
 
@@ -17,20 +19,47 @@ class ERC681 {
 
   factory ERC681.fromCommonEthereumURIData(CommonEthereumURIData data) {
     final query = Map.fromEntries(data.query);
-    return ERC681()
+
+    final erc681 = ERC681()
       ..scheme = data.scheme
       ..prefix = data.prefix
       ..chainId = data.chainId
       ..function = data.function
       ..address = data.address
-      ..valid = data.valid
-      ..gasLimit = (query['gas'] ?? query['gasLimit'])?.toBigInteger()
-      ..gasPrice = query['gasPrice']?.toBigInteger()
-      ..value = query['value']?.split('-').first.toBigInteger()
+      ..valid = data.valid;
+
+    BigInt? toBigInteger(String? value) {
+      if (value == null) {
+        return null;
+      }
+      if (!_scientificNumberRegEx.hasMatch(value)) {
+        erc681.valid = false;
+        return null;
+      }
+      if (value.contains('e')) {
+        final parts = value.split('e');
+        final base = parts[0].asRational;
+        final exponent = BigInt.tryParse(parts[1]) ?? BigInt.one;
+        return (base * (BigInt.from(10).pow(exponent.toInt()).toRational()))
+            .toBigInt();
+      }
+      final result = BigInt.tryParse(value);
+      if (result == null) {
+        erc681.valid = false;
+      }
+      return result;
+    }
+
+    erc681
+      ..gasLimit = toBigInteger((query['gas'] ?? query['gasLimit']))
+      ..gasPrice = toBigInteger(query['gasPrice'])
+      ..value = toBigInteger(query['value']?.split('-').first)
       ..functionParams = data.query
           .where((element) => element.key != 'gas' && element.key != 'value')
           .toList()
       ..amount = query['amount']?.asRationalOrNull;
+
+    return erc681;
   }
 
   bool valid = true;
@@ -46,8 +75,4 @@ class ERC681 {
 
   // extra adapted field
   Rational? amount;
-}
-
-extension _StringExt on String {
-  BigInt toBigInteger() => BigInt.parse(this);
 }
--- test/external_transfer_uri_parser_test.dart
+++ test/external_transfer_uri_parser_test.dart
@@ -24,6 +24,91 @@ void main() {
     expect(await _parse(urls[2]), null);
     expect(await _parse(urls[3]), null);
   });
+
+  test('parse ethereum uri', () async {
+    const url1 =
+        'ethereum:0xfb6916095ca1df60bb79Ce92ce3ea74c37c5d359?value=2.014e18';
+    final result1 = await _parse(url1);
+    expect(result1, isNotNull);
+
+    expect(result1!.assetId, ChainId.ethereum);
+    expect(result1.destination, '0xfb6916095ca1df60bb79Ce92ce3ea74c37c5d359');
+    expect(result1.amount, '2.014');
+
+    const url2 =
+        'ethereum:pay-0xdAC17F958D2ee523a2206206994597C13D831ec7@1/transfer?address=0x00d02d4A148bCcc66C6de20C4EB1CbAB4298cDcc&uint256=2e7&gasPrice=14';
+    final result2 = await _parse(url2);
+    expect(result2, isNotNull);
+
+    expect(result2!.assetId, '4d8c508b-91c5-375b-92b0-ee702ed2dac5');
+    expect(result2.destination, '0x00d02d4A148bCcc66C6de20C4EB1CbAB4298cDcc');
+    expect(result2.amount, '20');
+
+    const url3 =
+        'ethereum:0xD994790d2905b073c438457c9b8933C0148862db@1?value=1.697e16&gasPrice=14&label=Bitrefill%2008cba4ee-b6cd-47c8-9768-c82959c0edce';
+    final result3 = await _parse(url3);
+    expect(result3, isNotNull);
+
+    expect(result3!.assetId, ChainId.ethereum);
+    expect(result3.destination, '0xD994790d2905b073c438457c9b8933C0148862db');
+    expect(result3.amount, '0.01697');
+
+    const url4 =
+        'ethereum:0xA974c709cFb4566686553a20790685A47acEAA33@1/transfer?address=0xB38F2E40e82F0AE5613D55203d84953aE4d5181B&amount=1.24&uint256=1.24e18';
+    final result4 = await _parse(url4);
+    expect(result4, isNotNull);
+
+    expect(result4!.assetId, 'c94ac88f-4671-3976-b60a-09064f1811e8');
+    expect(result4.destination, '0xB38F2E40e82F0AE5613D55203d84953aE4d5181B');
+    expect(result4.amount, '1.24');
+
+    const url5 =
+        'ethereum:pay-0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48@1/transfer?address=0x50bF16E33E892F1c9Aa7C7FfBaF710E971b86Dd1&gasPrice=14';
+    final result5 = await _parse(url5);
+    expect(result5, isNull);
+
+    const url6 =
+        'ethereum:0xA974c709cFb4566686553a20790685A47acEAA33@1/transfer?a=b&c=d&uint256=1.24e18&e=f&amount=1.24&g=h&address=0xB38F2E40e82F0AE5613D55203d84953aE4d5181B&i=j&k=m&n=o&p=q';
+    final result6 = await _parse(url6);
+    expect(result6, isNotNull);
+
+    expect(result6!.assetId, 'c94ac88f-4671-3976-b60a-09064f1811e8');
+    expect(result6.destination, '0xB38F2E40e82F0AE5613D55203d84953aE4d5181B');
+    expect(result6.amount, '1.24');
+
+    const url7 =
+        'ethereum:0xA974c709cFb4566686553a20790685A47acEAA33@1/transfer?address=0xB38F2E40e82F0AE5613D55203d84953aE4d5181B&amount=1e7&uint256=1.24e18';
+    final result7 = await _parse(url7);
+    expect(result7, isNull);
+
+    const url8 =
+        'ethereum:0x20269e75b1637632e87f65A0A053d6720A781f39?amount=0.00016882';
+    final result8 = await _parse(url8);
+    expect(result8, isNotNull);
+
+    expect(result8!.assetId, ChainId.ethereum);
+    expect(result8.destination, '0x20269e75b1637632e87f65A0A053d6720A781f39');
+    expect(result8.amount, '0.00016882');
+
+    const url9 =
+        'ethereum:0xfb6916095ca1df60bb79Ce92ce3ea74c37c5d359?value=2.014e18&amount=2.014';
+    final result9 = await _parse(url9);
+    expect(result9, isNotNull);
+
+    expect(result9!.assetId, ChainId.ethereum);
+    expect(result9.destination, '0xfb6916095ca1df60bb79Ce92ce3ea74c37c5d359');
+    expect(result9.amount, '2.014');
+
+    const url10 =
+        'ethereum:0xfb6916095ca1df60bb79Ce92ce3ea74c37c5d359?value=2.014e18&amount=2.013';
+    final result10 = await _parse(url10);
+    expect(result10, isNull);
+
+    const url11 =
+        'ethereum:0xA974c709cFb4566686553a20790685A47acEAA33@1/transfer?address=0xB38F2E40e82F0AE5613D55203d84953aE4d5181B&amount=1&uint256=1.24e18';
+    final result11 = await _parse(url11);
+    expect(result11, isNull);
+  });
 }
 
 Future<ExternalTransfer?> _parse(String uri) => parseExternalTransferUri(
@@ -46,7 +131,7 @@ Future<ExternalTransfer?> _parse(String uri) => parseExternalTransferUri(
           chainId: ChainId.ethereum,
           precision: const {
                 '4d8c508b-91c5-375b-92b0-ee702ed2dac5': 6, // ERC20 USDT
-                'c94ac88f-4671-3976-b60a-09064f1811e8': 8, // XIN
+                'c94ac88f-4671-3976-b60a-09064f1811e8': 18, // XIN
               }[assetId] ??
               0),
     );
